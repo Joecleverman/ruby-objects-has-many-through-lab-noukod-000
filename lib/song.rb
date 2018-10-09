58800ca234949b1436c5bc76268d9298ab7bac3f
Josue Abraham <josuea@noukod.com>
2018-10-09 19:15:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,11 @@
 class Song
-
-  attr_reader :name
-  attr_accessor :artist, :genre
-
-  def initialize(name, genre)
-    @name = name
-    @genre = genre
-    genre.add_song(self)
-  end
-
-end
+	attr_accessor :name, :genre, :artist
+ 	def initialize(name, genre)
+		@name = name
+		@genre = genre
+		genre.add_song(self)
+	end
+ 	def add_artist(artist)
+		artist.songs << self
+	end
+end 
