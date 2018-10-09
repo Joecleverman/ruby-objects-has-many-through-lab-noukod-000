d8bc9cb32c4ea5b2e529ae0d0dea8ba13436c8cf
Josue Abraham <josuea@noukod.com>
2018-10-09 19:14:01 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,17 +1,17 @@
 class Doctor
-  attr_reader :name, :appointments
-  def initialize(name)
-    @name = name
-    @appointments = []
-  end
-
-  def add_appointment(appointment)
-    self.appointments << appointment
-    appointment.doctor = self
-  end
-
-  def patients
-    self.appointments.map {|a| a.patient}
-  end
-
-end
+	attr_accessor :name
+ 	def initialize(name)
+		@name = name
+		@appointments = []
+	end
+ 	def add_appointment(appointment)
+		@appointments << appointment
+		appointment.doctor = self
+	end
+ 	def appointments
+		@appointments
+	end
+ 	def patients
+		self.appointments.collect { |appointment| appointment.patient }
+	end
+end 
